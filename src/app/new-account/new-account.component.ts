3e37ff96350cb0a22e6ac22afdd4f8cd513a6d4c
zivkoss <zivcoss@yahoo.com>
2023-05-03 12:08:50 +0200

    Project added

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+// import { LoggingService } from '../logging.service'; // there is a better way to do this with angular*
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) {}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+    });
+    this.loggingService.logStatusChange(accountStatus);
+    // const service = new LoggingService(); //*
+    // service.logStatusChange(accountStatus); //*
+  }
+}
